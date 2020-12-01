1a86e220e4651496a1f25cc2b1373bcc4c79ec85
Felipe Arias Russi <af.ariasr@uniandes.edu.co>
2020-11-30 21:08:05 -0500

    Cambio del view

--- src/view/View.java
+++ src/view/View.java
@@ -14,7 +14,7 @@ public class View {
 		System.out.println("3. Cantidad de clusters");
 		System.out.println("5. Estaciones criticas");
 		System.out.println("6. Ruta por resistencia");		
-		System.out.println("7. Estacion con mas viajes por edad FALTA LA RUTA MAS CORTA CON DIJKSTRA");
+		System.out.println("7. Estacion con mas viajes por edad + recorrido.");
 		System.out.println("8. Ruta interes turistico");
 		System.out.println("9. Imprimir mapa");
 	}
